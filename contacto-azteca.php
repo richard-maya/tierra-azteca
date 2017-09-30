bbf48288560be54f1db0f432af83acfe733bb7bf
Richard Maya <richard.maya94@gmail.com>
2017-09-30 13:06:48 -0500

    Atún 30 Septiembre

---
+++ contacto-azteca.php
@@ -0,0 +1,331 @@
+<?php $form_path='assets/formoid/form.php'; require_once $form_path; ?><!DOCTYPE html>
+<!--**********************************************************************
+* FILENAME :        TIERRA AZTECA | Azteca Contact
+*
+* DESCRIPTION :
+*       Customer support.
+*
+*       Copyright Tierra Azteca © All rights reserved.
+*
+* AUTHOR :    Richard Maya
+* START DATE :    07 Jul 17
+*
+* VERSION : 1.0
+*
+*-->
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Cache-control" content="public, max-age:1209600" />
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta name="copyright" content="Copyright © 2017 Tierra Azteca">
+
+    <!-- SITE TITLE
+    ================================================== -->
+    <title>Alimentación saludable. ¡Consúltanos y te diremos cómo!</title>
+
+    <!-- FAVICONS
+    ================================================== -->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+    <link rel="manifest" href="favicons/manifest.json">
+    <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- SEO INFO
+    ================================================== -->
+    <meta name="description" content="Espacio saludable con alimentación ligera. ¡Consulta las ubicaciones!">
+    <meta name="keywords" content="máquinas vending, vending toluca, vending méxico, alimentación sana, dieta equilibrada">
+    <meta name="author" content="Richard Maya">
+    <meta name="designer" content="Richard Maya">
+    <meta name="geo.region" content="MX" />
+    <meta name="geo.placename" content="San Fernando Huixquilucan" />
+    <meta name="geo.position" content="19.38746;-99.282212" />
+    <meta name="ICBM" content="19.38746, -99.282212" />
+
+    <!-- STYLESHEETS
+    ================================================== -->
+    <link rel="stylesheet" href="assets/tether/css/tether.min.css">
+    <link rel="stylesheet" href="assets/animate/animate.min.css">
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+
+    <!-- NAV BAR
+    ================================================== -->
+    <section id="blue-logo" class="d-flex align-items-end hidden-sm-down">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 d-flex align-items-center">
+                    <img src="gph/grupo-azteca.png" alt="" class="img-fluid">
+                </div>
+                <nav class="col-12 col-md-10 d-flex justify-content-end">
+                    <a href="index.html">Inicio</a>
+                    <a href="tierra-azteca/productos-azteca.html">Productos</a>
+                    <a href="tierra-vending/tierra-vending.html">Máquinas</a>
+                    <a href="tierra-azteca.html">Nosotros</a>
+                    <a href="contacto-azteca.php" class="active">Contacto</a>
+                </nav>
+            </div>
+        </div>
+    </section>
+
+    <nav class="navbar navbar-toggleable-md bg-faded sticky-top bg-primary navbar-inverse hidden-md-up" style="background-color:#04ACDC !important;">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+            data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+            aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="index.html">
+            <img src="gph/grupo-azteca.png" width="auto" height="50" class="d-inline-block align-top" alt="">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tierra-azteca/productos-azteca.html">Productos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tierra-vending/tierra-vending.html">Máquinas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tierra-azteca.html">Nosotros</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="contacto-azteca.php">Contacto <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+<!-- HEADING
+================================================== -->
+<section id="contact-heading" class="parallax-window d-flex align-items-center wow fadeIn" data-parallax="scroll" data-image-src="img/headings/contact-heading.jpg">
+    <div class="container">
+        <div class="row">
+            <header class="wow fadeInUp">
+                <h1 class="grupoa-heading text-center wow">
+                    <span class="azteca-heading-white">Tierra Azteca</span> <br> Contacto
+                </h1>
+            </header>
+        </div>
+    </div>
+</section>
+
+
+<!-- BLUE SLOGAN
+================================================== -->
+<section id="blue-slogan">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <h3 class="azteca-subtitle wow fadeInUp font-italic">Productos saludables de alta calidad a precios competitivos</h3>
+        </div>
+    </div>
+</section>
+
+
+<!-- PROCESS
+================================================== -->
+<section id="process">
+    <center>
+        <h2 class="grupoa-title text-center wow fadeInDown">¿Cómo lo hacemos?</h2>
+        <small class="wow fadeInDown">En 3 sencillos pasos usted y su empresa podrán comenzar a gozar de los beneficios de tener un equipo Vending en sus instalaciones.</small>
+    </center><br>
+    <div class="container">
+        <div class="row wow fadeInUp">
+
+            <!-- Call -->
+            <div class="col-md-4">
+                <div class="card">
+                    <center>
+                        <img class="card-img-top" src="img/index-features/call-answer.png" alt="freepik-call-answer-vector">
+                    </center>
+                    <div class="card-block">
+                        <p class="card-text">Usted nos llama.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Brief -->
+            <div class="col-md-4 text-xs-center">
+                <div class="card">
+                    <center>
+                        <img class="card-img-top" src="img/index-features/specialist-user.png" alt="freepik-specialist-user-vector">
+                    </center>
+                    <div class="card-block">
+                        <p class="card-text">Uno de nuestros representantes lo visitará SIN NINGÚN COMPROMISO y le orientará acerca de la solución Vending ideal para su negocio.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Deal -->
+            <div class="col-md-4 text-xs-center">
+                <div class="card">
+                    <center>
+                        <img class="card-img-top" src="img/index-features/contract.png" alt="freepik-contract-vector">
+                    </center>
+                    <div class="card-block">
+                        <p class="card-text">Se firma el respectivo contrato y listo, usted empezará a gozar de los beneficios de éste sector comercial.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- CONTACT FORM & MAP
+================================================== -->
+<section id="contacto">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 offset-md-3">
+                <h2 class="grupoa-title d-flex justify-content-center wow fadeInDown" id="azteca-contact-form">Contáctanos</h2>
+
+                <form class="formoid-solid-blue" method="post">
+                    <div class="row">
+
+                        <!-- Nombre -->
+                        <div class="form-group col-6 wow fadeInUp">
+                            <label for="nombre">Nombre *</label>
+                            <input placeholder="Nombre" type="text" name="name[first]" required="required" class="form-control" id="nombre">
+                        </div>
+
+                        <!-- Apellido -->
+                        <div class="form-group col-6 wow fadeInUp">
+                            <label for="apellido">Apellido *</label>
+                            <input placeholder="Apellido" type="text" name="name[last]" required="required" class="form-control" id="apellido">
+                        </div>
+                    </div>
+
+                    <div class="row">
+
+                        <!-- Teléfono -->
+                        <div class="form-group col-6 wow fadeInUp">
+                            <label for="telefono">Teléfono *</label>
+                            <input type="tel" class="form-control" maxlength="24" name="phone" required="required" placeholder="Teléfono">
+                        </div>
+
+                        <!-- Empresa -->
+                        <div class="form-group col-6 wow fadeInUp">
+                            <label for="empresa">Empresa *</label>
+                            <input type="text" class="form-control" name="input" required="required" placeholder="Empresa">
+                        </div>
+                    </div>
+
+                    <!-- Correo Electrónico -->
+                    <div class="row">
+                        <div class="form-group col-12 wow fadeInUp">
+                            <label for="email">Correo Electrónico</label>
+                            <input type="email" class="form-control" name="email" placeholder="Correo Electrónico">
+                        </div>
+                    </div>
+
+                    <!-- Mensaje -->
+                    <div class="row">
+                        <div class="form-group col-12 wow fadeInUp">
+                            <label for="textarea">Mensaje *</label>
+                            <textarea name="textarea" class="form-control" id="message" cols="20" rows="5" required="required" placeholder="Mensaje"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="row d-flex justify-content-center wow fadeInUp">
+                        <button class="btn btn-grupoa-outline" type="submit">Enviar Mensaje</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- FOOTER
+================================================== -->
+<footer class="grupoa-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-10 offset-1 col-md-6 offset-md-3 wow fadeInUp">
+                <div class="row justify-content-center">
+                    <div class="col-3 hidden-sm-down">
+                        <img src="gph/chat-index.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-9">
+                        <h5>Contáctanos</h5>
+                        <p class="azteca-footer">Tel: <a href="tel://55-5815-0387">(52) 55 5815-0387</a> <br>
+                            E-mail: <a href="mailto:contacto@tierra-azteca.com">contacto@tierra-azteca.com</a></p>
+                        <a href="contacto-azteca.php#azteca-contact-form" class="azteca-footer">Escríbenos un mensaje</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Site Map -->
+        <div class="row justify-content-center">
+            <ul class="nav nav-justified wow fadeInUp">
+                <li>
+                    <a href="index.html" class="azteca-footer">Inicio</a>
+                    <span class="hidden-sm-down">|</span><br class="hidden-md-up">
+                    <a href="tierra-azteca/productos-azteca.html" class="azteca-footer">Productos Tierra Azteca</a>
+                    <span class="hidden-sm-down">|</span><br class="hidden-md-up">
+                    <a href="tierra-vending/tierra-vending.html" class="azteca-footer">Máquinas Vending</a>
+                    <span class="hidden-sm-down">|</span><br class="hidden-md-up">
+                    <a href="tierra-azteca.html" class="azteca-footer">Nosotros</a>
+                    <span class="hidden-sm-down">|</span><br class="hidden-md-up">
+                    <a href="tierra-azteca.html" class="azteca-footer">Aviso de Privacidad</a>
+                    <span class="hidden-sm-down">|</span><br class="hidden-md-up">
+                    <a href="contacto-azteca.php" class="azteca-footer">Contacto</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="row justify-content-center wow fadeInUp">
+            <p class="text-center" id="copyright">© Tierra Azteca 2017. Todos los derechos reservados.</p>
+        </div>
+
+        <div class="row justify-content-center wow fadeInUp">
+            <p class="text-center" id="developer">Desarrollado por <a href="http://meraki-creative.studio" rel="author" target="_blank">Meraki Creative Studio</a><a href="http://meraki-creative.studio" rel="author" target="_blank" style="display: none; visibility: hidden;">branding, fotografía, motion graphics, video, animación, toluca, metepec</a></p>
+        </div>
+    </div>
+</footer>
+
+
+<!-- SCRIPTS
+================================================== -->
+<script src="assets/jquery/jquery.min.js"></script>
+<script src="assets/tether/js/tether.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/parallax/parallax.min.js"></script>
+<script src="assets/wow/wow.min.js"></script>
+<script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+
+<script type="text/javascript">
+var wow = new WOW(
+  {
+    boxClass:     'wow',
+    animateClass: 'animated',
+    offset:       50,
+    mobile:       false,
+    live:         true,
+    callback:     function(box) {
+    },
+    scrollContainer: null
+  }
+);
+wow.init();
+
+$('.parallax-window').parallax({
+    imageSrc: 'img/headings/contact-heading.jpg',
+    positionY: 'top'
+});
+</script>
+
+</body>
+</html>
